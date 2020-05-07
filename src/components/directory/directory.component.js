50181c09bdfc1b55ea86c4ca80ce33fd30b1c3e7
Thanh Chung Bui <tbui@schoo.jp>
2020-05-07 17:44:09 +0900

    setup header and more

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -46,8 +46,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map((section) => (
-          <MenuItem {...section} />
+        {this.state.sections.map(({id, ...others}) => (
+          <MenuItem key={id} {...others} />
         ))}
       </div>
     );
