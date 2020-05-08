f9d4f027a9efda0df400f899614a584c2173c60f
Thanh Chung Bui <thanhchungbtc@gmail.com>
2020-05-08 18:32:28 +0900

    update config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDTCdqASxPeGYi8pWdwIUN57JRC_FAhj8c",
+  authDomain: "myjp-8ca32.firebaseapp.com",
+  databaseURL: "https://myjp-8ca32.firebaseio.com",
+  projectId: "myjp-8ca32",
+  storageBucket: "myjp-8ca32.appspot.com",
+  messagingSenderId: "93511095659",
+  appId: "1:93511095659:web:85e2807c6546e02958682e",
+}
