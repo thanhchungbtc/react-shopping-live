9577e4d80e1e0ec1ee05e2346795380f7701622c
Thanh Chung Bui <tbui@schoo.jp>
2020-05-08 11:41:22 +0900

    cart logic

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,11 +1,20 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
-import './cart-dropdown.styles.scss'
-const CartDropdown = () => (
+import "./cart-dropdown.styles.scss";
+import { connect } from "react-redux";
+import CartItem from "../cart-item/cart-item.component";
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {cartItems.map((item) => {
+        return <CartItem key={item.id} item={item} />;
+      })}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+export default connect(mapStateToProps, null)(CartDropdown);
