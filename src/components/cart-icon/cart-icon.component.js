f7833876b74e1f7554b119a7bbee248ea9063048
Thanh Chung Bui <thanhchungbtc@gmail.com>
2020-05-08 14:31:45 +0900

    add reselector

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -3,15 +3,19 @@ import React from "react";
 import "./cart-icon.styles.scss";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <span className="shopping-icon">Cart</span>
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
 const mapDispatchTopProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
-export default connect(null, mapDispatchTopProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchTopProps)(CartIcon);
