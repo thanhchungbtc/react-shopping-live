f27f53352afc01e434ed07b0d66ed560ed96618f
Thanh Chung Bui <thanhchungbtc@gmail.com>
2020-05-08 16:24:50 +0900

    persist tore

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-
       <BrowserRouter>
-        <App/>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
